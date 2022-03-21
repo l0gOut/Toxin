ca496dd2105e9d98eb07bc9f68bf6d9e5f3cc541
l0gOut <daniil.petrunin2013@yandex.ru>
2022-03-22 00:37:03 +0300

    create bug file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,6 +17,9 @@ module.exports = {
       template: path.resolve(__dirname, "./src/index.pug"),
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: `[contenthash].css`,
+    }),
     new FaviconsWebpackPlugin({
       logo: "./src/assets/image/favicon.ico",
       icons: {
@@ -25,31 +31,18 @@ module.exports = {
         yandex: true,
       },
     }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `src/assets/img`, to: `assets/img` },
+        { from: `src/assets/fonts`, to: `assets/fonts` },
+      ],
+    }),
+    new ESLintPlugin(),
     new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
-      {
-        test: /\.pug$/i,
-        use: {
-          loader: "pug-loader",
-          options: {
-            pretty: process.env.NODE_ENV === "development",
-          },
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
@@ -59,6 +52,61 @@ module.exports = {
             presets: ["@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.pug$/i,
+        use: {
+          loader: "pug-loader",
+          options: {
+            pretty: process.env.NODE_ENV === "development",
+          },
+        },
+      },
+
+      {
+        test: /\.(png|jpg|svg|jpeg)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets",
+        },
+      },
+      {
+        test: /\.(ttg|woff|woff2)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              import: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+        ],
       },
     ],
   },
