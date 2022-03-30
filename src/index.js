40d223e9fcf77e54b8247a8b76fc1f62222dd800
l0gOut <daniil.petrunin2013@yandex.ru>
2022-03-30 15:39:32 +0300

    fix webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./scss/style.scss";
 import "./scss/header.scss";
+
+__webpack_public_path__ = process.env.ASSET_PATH;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
@@ -11,17 +9,16 @@ module.exports = {
   output: {
     filename: "[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext]",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.pug"),
+      template: path.resolve(__dirname, "src/index.pug"),
       filename: "index.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: `[contenthash].css`,
-    }),
+
     new FaviconsWebpackPlugin({
-      logo: "./src/assets/image/favicon.ico",
+      logo: path.resolve(__dirname, "src/assets/image/favicon.ico"),
       icons: {
         android: true,
         appleIcon: true,
@@ -30,13 +27,6 @@ module.exports = {
         windows: true,
         yandex: true,
       },
-    }),
-
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: `src/assets/img`, to: `assets/img` },
-        { from: `src/assets/fonts`, to: `assets/fonts` },
-      ],
     }),
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
@@ -65,23 +55,16 @@ module.exports = {
 
       {
         test: /\.(png|jpg|svg|jpeg)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets",
-        },
+        type: "asset/resource",
       },
       {
-        test: /\.(ttg|woff|woff2)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets",
-        },
+        test: /\.(ttf|ttg|woff|woff2)$/i,
+        type: "asset/resource",
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           "style-loader",
-          MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: {
@@ -93,18 +76,6 @@ module.exports = {
             options: {
               sourceMap: true,
             },
-          },
-        ],
-      },
-
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { sourceMap: true },
           },
         ],
       },
